d615927468b4e469c2b1ecc3dcd59d4796cd3aab
howsun <howardsun509@gmail.com>
2018-12-08 19:43:49 -0800

    fix config for heroku

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from configvar import secret_key, db_setting
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = secret_key
-    SQLALCHEMY_DATABASE_URI = db_setting['uri']
-    SQLALCHEMY_TRACK_MODIFICATIONS = db_setting['mod']
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secret_key
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or db_setting
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
